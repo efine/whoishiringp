06a06587def925ef1e550c1e427eef2ab22d2c8f
Edwin Fine <efine@users.noreply.github.com>
2019-04-26 00:40:43 -0400

    Initial creation

---
+++ elixir/whoishiringp.ex
@@ -0,0 +1,175 @@
+defmodule WhoIsHiring do
+
+    def main(max_concurrency) do
+        url_base = "https://hacker-news.firebaseio.com/v0"
+        user_id = "whoishiring"
+        
+        Application.ensure_all_started(:inets)
+        Application.ensure_all_started(:ssl)
+        user = get_user(url_base, user_id)
+        case lookup(user, "submitted") do
+            nil ->
+                IO.puts("No submissions for user ~p\n", [User]);
+            submissions ->
+                print_jobs(url_base, submissions, max_concurrency)
+        end
+    end
+    
+    
+    def print_jobs(jobs) do
+        numbered_jobs = [Enum.to_list(1..length(jobs)), jobs] |> List.zip()
+        for {n, job} <- numbered_jobs, do: IO.puts("##{n}: #{job}\n")
+        :ok
+    end
+    
+    def print_jobs(url_base, submissions, max_concurrency) do
+        case get_first_matching_submission(url_base, submissions) do
+            {:ok, kw} ->
+                title = lookup(kw, "title", "")
+                kids = lookup(kw, "kids", [])
+                IO.puts(title)
+                pget_jobs(url_base, kids, max_concurrency)
+                |> Enum.reject(&(!&1))
+                |> print_jobs()
+            _Else ->
+                :ok
+        end
+    end
+
+
+    def get_first_matching_submission(url_base, [id|submitted]) do
+        submission = get_url_json(url_base, "item", id)
+        case is_matching_submission(submission) do
+            true ->
+                {:ok, submission}
+            false ->
+                get_first_matching_submission(url_base, submitted)
+        end
+    end
+    def get_first_matching_submission(_url_base, []) do
+        nil
+    end
+    
+    def is_matching_submission(nil), do: false
+    def is_matching_submission(submission) do
+        title = lookup(submission, "title", "")
+        type = lookup(submission, "type", "")
+        title =~ ~r/Ask\s+HN:\s+Who\s+is\s+hiring/i and type == "story"
+    end
+    
+    def pget_jobs(url_base, kids, chunk_size) do
+        IO.puts("Found #{length(kids)} jobs to search\n")
+        pmap_n(WhoIsHiring, :get_job, [url_base], kids, chunk_size)
+    end
+
+    def pmap_n(mod, func_name, extra_args, list, max_chunk, results \\ [], n \\ 0)
+    def pmap_n(_, _, _, [], _, results, n) do
+        :io.format(:standard_error, '\r~8..0B\n', [n])
+        results
+        |> Enum.reject(&(&1 == nil))
+        |> Enum.reverse()
+    end
+    def pmap_n(mod, func_name, extra_args, list, max_chunk, results, n) do
+        case Enum.split(list, max_chunk) do
+            {[_|_]=todo, rest} ->
+                rev_results = pmap(mod, func_name, extra_args, todo)
+                              |> Enum.reverse()              
+                n1 = n + length(todo)
+                :io.format(:standard_error, '~8..0B\r', [n1])
+                pmap_n(mod, func_name, extra_args, rest, max_chunk,
+                       rev_results ++ results, n1)
+            {[], rest} ->
+                pmap_n(mod, func_name, extra_args, rest, max_chunk, results, n)
+        end
+    end
+
+    def pmap(mod, func_name, extra_args, args) do
+        results = for arg <- args do
+            Task.async(mod, func_name, [arg|extra_args])
+        end
+        |> Task.yield_many(30000)
+        |> Enum.map(fn {task, result} ->
+               result || Task.shutdown(task, :brutal_kill)
+           end)
+        for {:ok, result} <- results, do: result
+    end
+
+    # #Synchronous http call
+    def get_url_json(base, resource, id, timeout \\ 30000) do
+        {:ok, request_id} = get_url_json_async(base, resource, id)
+
+        case receive_response(request_id, timeout) do
+            {:ok, response} -> response
+            {:error, _} -> nil
+        end
+    end
+    
+    
+    # Asynchronous http call
+    def get_url_json_async(base, resource, id) do
+        url = make_url(base, resource, id)
+        opts = [{:body_format, :binary}, {:sync, :false}]
+        :httpc.request(:get, {url, []}, [], opts)
+    end
+
+    def receive_response(request_id, timeout) do
+        receive do
+            {:http, {^request_id, result}} ->
+                {{_version, 200, _reason_phrase}, _headers, body} = result
+                {:ok, :jsx.decode(body)}
+        after
+            timeout ->
+                {:error, :timeout}
+        end        
+    end
+    
+    def make_url(base, resource, id) do
+        "#{base}/#{resource}/#{id}.json" |> to_charlist
+    end
+    
+    def check_remote_job(nil), do: false
+    def check_remote_job(text), do: text =~ ~r/remote/i && text
+    
+    def get_job(id, url_base) when is_integer(id) do
+        get_url_json(url_base, "item", id)
+        |> get_text()
+        |> check_remote_job()
+    end
+    
+    
+    def get_text(kid) when is_list(kid) do
+        case {lookup(kid, "text"), lookup(kid, "deleted")} do
+            {_, true} ->
+                nil
+            {nil, _} ->
+                IO.puts("No text in #{inspect kid}\n")
+                nil
+            {text, _} ->
+                text
+        end
+    end
+    def get_text(_) do
+        nil
+    end
+    
+    def get_user(url_base, user_id) do
+        user = get_url_json(url_base, "user", user_id)
+        id = lookup(user, "id")
+        case id == user_id do
+            false ->
+                msg = "Expected user id [#{user_id}] in #{inspect user}, got [#{id}], aborting"
+                IO.puts(msg)
+                throw(msg)
+            true ->
+                user
+        end
+    end
+
+    def lookup(kvs, k, default \\ nil) when is_list(kvs) do
+        case List.keyfind(kvs, k, 0) do
+            nil -> default
+            tval -> elem(tval, 1)
+        end
+    end
+
+end
---
+++ erlang/whoishiringp.erl
@@ -0,0 +1,196 @@
+-module(whoshiringp).
+
+-export([main/0]).
+-export([main/1]).
+
+-define(URL_BASE, "https://hacker-news.firebaseio.com/v0").
+-define(USER_ID, <<"whoishiring">>).
+-define(MAX_CONCURRENCY, (8 * num_cpus())).
+
+
+-export([get_job/2, num_cpus/0]).
+
+main() ->
+    main(?MAX_CONCURRENCY).
+
+main(MaxConcurrency) ->
+    inets:start(),
+    ssl:start(),
+    User = get_user(?URL_BASE, ?USER_ID),
+    case proplists:get_value(<<"submitted">>, User) of
+        undefined ->
+            io:format("No submissions for user ~p\n", [User]);
+        Submissions ->
+            print_jobs(?URL_BASE, Submissions, MaxConcurrency)
+    end.
+
+get_user(UrlBase, UserId) ->
+    User = get_url_json(UrlBase, "user", UserId),
+    Id = proplists:get_value(<<"id">>, User),
+    case Id =:= UserId of
+        false ->
+            io:format("Expected user id ~p, got ~p, aborting\n", [UserId, Id]),
+            erlang:halt(1);
+        true ->
+            User
+    end.
+
+print_jobs(UrlBase, Submissions, MaxConcurrency) ->
+    case get_first_matching_submission(UrlBase, Submissions) of
+        {ok, Props} ->
+            Title = proplists:get_value(<<"title">>, Props, <<"">>),
+            Kids = proplists:get_value(<<"kids">>, Props, []),
+            io:format("~s\n", [Title]),
+            Jobs = pget_jobs(UrlBase, Kids, MaxConcurrency),
+            print_jobs(Jobs);
+        _Else ->
+            ok
+    end.
+
+
+get_first_matching_submission(UrlBase, [Id|Submitted]) ->
+    Submission = get_url_json(UrlBase, "item", Id),
+    case is_matching_submission(Submission) of
+        true->
+            {ok, Submission};
+        false ->
+            get_first_matching_submission(UrlBase, Submitted)
+    end;
+get_first_matching_submission(_UrlBase, []) ->
+    undefined.
+
+is_matching_submission(Submission) ->
+    Title = proplists:get_value(<<"title">>, Submission, <<"">>),
+    Type = proplists:get_value(<<"type">>, Submission, <<"">>),
+    Opts = [{capture, none}, caseless],
+    case re:run(to_s(Title), "Ask\\s+HN:\\s+Who\\s+is\\s+hiring", Opts) of
+        match ->
+            Type =:= <<"story">>;
+        _Else ->
+            false
+    end.
+
+pget_jobs(UrlBase, Kids, MaxConcurrency) ->
+    Njobs = length(Kids),
+    io:format("Found ~p jobs to search\n", [Njobs]),
+    ChunkSize = MaxConcurrency,
+    pmap_n({?MODULE, get_job}, [UrlBase], Kids, ChunkSize).
+
+print_jobs(Jobs) ->
+    NumberedJobs = lists:zip(lists:seq(1, length(Jobs)), Jobs),
+    [io:format("#~B: ~s\n", [N, Job]) || {N, Job} <- NumberedJobs],
+    ok.
+
+
+make_url(Base, Resource, Id) ->
+    to_s(iolist_to_binary([Base, $/, Resource, $/, to_s(Id), ".json"])).
+
+%% Synchronous http call
+get_url_json(Base, Resource, Id) ->
+    {ok, RequestId} = get_url_json_async(Base, Resource, Id),
+    {ok, Response} = receive_response(RequestId, 30000),
+    Response.
+
+%% Asynchronous http call
+get_url_json_async(Base, Resource, Id) ->
+    Url = make_url(Base, Resource, Id),
+    Opts = [{body_format, binary}, {sync, false}],
+    httpc:request(get, {Url, []}, [], Opts).
+
+
+receive_response(RequestId, Timeout) ->
+    receive
+        {http, {RequestId, Result}} ->
+            {{_Version, 200, _ReasonPhrase}, _Headers, Body} = Result,
+            {ok, jsx:decode(Body)}
+    after
+        Timeout ->
+            {error, timeout}
+    end.
+
+
+
+num_cpus() ->
+    case erlang:system_info(logical_processors_online) of
+        unknown -> 1;
+        N -> N
+    end.
+
+
+pmap_n(FuncSpec, ExtraArgs, List, MaxChunk) ->
+    pmap_n(FuncSpec, ExtraArgs, List, MaxChunk, [], 0).
+
+pmap_n(_, _, [], _, Results, N) ->
+    io:format(standard_error, "\r~8..0B\n", [N]),
+    lists:reverse([R || R <- Results, R /= undefined]);
+pmap_n(FuncSpec, ExtraArgs, List, MaxChunk, Results, N) ->
+    {ToDo, Rest} = safe_split(MaxChunk, List),
+    %io:format("~p\n", [{FuncSpec, ExtraArgs, ToDo}]),
+    case ToDo of
+        [_|_] ->
+            Result = safe_rpc_pmap(FuncSpec, ExtraArgs, ToDo),
+            NewN = N + length(Result),
+            io:format(standard_error, "~8..0B\r", [NewN]),
+            pmap_n(FuncSpec, ExtraArgs, Rest, MaxChunk,
+                   lists:reverse(Result) ++ Results, NewN);
+        [] ->
+            pmap_n(FuncSpec, ExtraArgs, Rest, MaxChunk, Results, N)
+    end.
+
+
+safe_rpc_pmap(_FuncSpec, _ExtraArgs, []) ->
+    [];
+safe_rpc_pmap(FuncSpec, ExtraArgs, L) ->
+    try
+        rpc:pmap(FuncSpec, ExtraArgs, L)
+    catch
+        _:badrpc ->
+            io:format(standard_error, "pmap failed for ExtraArgs=~p, L=~p\n",
+                      [ExtraArgs, L]),
+            %% Drop one item from list and retry until ok or empty
+            safe_rpc_pmap(FuncSpec, ExtraArgs, tl(L))
+    end.
+
+safe_split(N, L) when N =< length(L) ->
+    lists:split(N, L);
+safe_split(_N, L) ->
+    {L, []}.
+
+
+-spec get_job(Id, UrlBase) -> Result when
+    Id :: integer(), UrlBase :: binary(), Result :: undefined | string().
+get_job(Id, UrlBase) when is_integer(Id) ->
+    Kid = get_url_json(UrlBase, "item", Id),
+    check_remote_job(get_text(Kid)).
+
+
+get_text(null) ->
+    undefined;
+get_text(Kid) ->
+    case {proplists:get_value(<<"text">>, Kid),
+          proplists:get_value(<<"deleted">>, Kid)} of
+        {_, true} ->
+            undefined;
+        {undefined, _} ->
+            io:format("No text in ~p\n", [Kid]),
+            undefined;
+        {Text, _} ->
+            Text
+    end.
+
+check_remote_job(undefined) ->
+    undefined;
+check_remote_job(Text) ->
+    Opts = [{capture, none}, caseless],
+    case re:run(Text, "remote", Opts) of
+        match -> Text;
+        _Else -> undefined
+    end.
+
+
+to_s(<<B/bytes>>) ->
+    binary_to_list(B);
+to_s(I) when is_integer(I) ->
+    integer_to_list(I);
+to_s(S) when is_list(S) ->
+    S.
